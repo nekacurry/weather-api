e6e0fa2069d536ffa6d2f1cf15fc2f929893a93c
Anneka Curry <69550392+nekacurry@users.noreply.github.com>
2021-04-26 18:32:48 -0500

    finished challenges 1 and 2

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// Functions
+function getWeather(zip, apiKey, successCallback, units = 'imperial') {
+    const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
+    
+
+    fetch(path)
+        .then( res => res.json())
+        .then( json => successCallback(json))
+        .catch( err => console.log(err.message))
+
+}
+export {
+    getWeather
+}
