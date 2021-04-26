5c8508d72a48dbd89b144d0bb7e4fd4a4bf800b6
Anneka Curry <69550392+nekacurry@users.noreply.github.com>
2021-04-26 18:56:39 -0500

    completed challenge 5

--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ async function getWeather(zip, apiKey, units = 'imperial') {
     const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
     
     try {
+        // try something
         const res = await fetch(path)
         const json = await res.json()
-        return json
+        const weatherData = {
+            code: json.cod,
+            coord: json.coord,
+            name: json.name,
+            temp: json.main.temp,
+            feels_like: json.main.feels_like,
+            temp_min: json.main.temp_min,
+            temp_max: json.main.temp_max,
+            description: json.weather[0].description
+        }
+        return weatherData
     } catch(err) {
+        // handle errors
         return err
     }
 
